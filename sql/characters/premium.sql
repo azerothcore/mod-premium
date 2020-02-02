64f0f86f6fc97183d6b2af01740e1848e5b87f78
BarbzYHOOL <professor_barbz@gmx.com>
2020-02-03 00:35:30 +0100

    chore(sql): Changed utf8 to utf8mb4
    - Useless in that case, but who knows later?

--- sql/characters/premium.sql
+++ sql/characters/premium.sql
@@ -2,4 +2,4 @@ DROP TABLE IF EXISTS `mod_premium`;
 CREATE TABLE `mod_premium` (
   `account_id` int(11) unsigned NOT NULL,
   `active` tinyint(1) unsigned NOT NULL default '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
